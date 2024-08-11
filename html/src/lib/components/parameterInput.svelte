1a079b22ddc54c3a65ab046d6c22c978ffdd3a68
hiroshi ueda <hiroshi.ueda.career.9756@gmail.com>
2024-08-11 23:43:25 +0900

    Refactored: Run prettier.

--- html/src/lib/components/parameterInput.svelte
+++ html/src/lib/components/parameterInput.svelte
@@ -1,19 +1,19 @@
-<script lang='ts'>
-    export let parameterValue: number
-    export let step: number
-    export let disabled = false
+<script lang="ts">
+  export let parameterValue: number;
+  export let step: number;
+  export let disabled = false;
 </script>
 
 <div class="parameter">
-    <label>
-        <slot />
-        <input type="number" step={step} bind:value={parameterValue} disabled={disabled}>
-    </label>
+  <label>
+    <slot />
+    <input type="number" {step} bind:value={parameterValue} {disabled} />
+  </label>
 </div>
 
 <style>
-    .parameter input {
-        width: 8rem;
-        margin-left: 1rem;
-    }
+  .parameter input {
+    width: 8rem;
+    margin-left: 1rem;
+  }
 </style>
--- html/src/routes/+page.svelte
+++ html/src/routes/+page.svelte
@@ -1,178 +1,186 @@
-<script lang='ts'>
-	import type { ComponentEvents } from 'svelte';
-	import Image from '../lib/components/image.svelte'
-	import ParameterInput from "../lib/components/parameterInput.svelte";
-	import Snow from '../lib/components/snow.svelte'
-	import ThemeSwitch from "../lib/components/themeSwitch.svelte";
+<script lang="ts">
+  import type { ComponentEvents } from 'svelte';
+  import Image from '../lib/components/image.svelte';
+  import ParameterInput from '../lib/components/parameterInput.svelte';
+  import Snow from '../lib/components/snow.svelte';
+  import ThemeSwitch from '../lib/components/themeSwitch.svelte';
 
-	import BinaryImage from '../lib/modules/binaryImage'
-	import ImageRestoration from "../lib/modules/imageRestoration"
+  import BinaryImage from '../lib/modules/binaryImage';
+  import ImageRestoration from '../lib/modules/imageRestoration';
 
-	const imageSize = {
-		siteSize: 5,
-		width: 30,
-		height: 30,
-	}
+  const imageSize = {
+    siteSize: 5,
+    width: 30,
+    height: 30
+  };
 
-	let degradeProbability = 0.1
-	let isThemeSwitched = false
+  let degradeProbability = 0.1;
+  let isThemeSwitched = false;
 
-	$: nishimoriInverseTemperature = ImageRestoration.getNishimoriInverseTemperature(degradeProbability)
-	$: iterationCount
+  $: nishimoriInverseTemperature =
+    ImageRestoration.getNishimoriInverseTemperature(degradeProbability);
+  $: iterationCount;
 
-	let {
-		restorationInverseTemperature,
-		exchangeConstant,
-		errorThreshold,
-		iterationCount
-	} = ImageRestoration.DefaultParameters()
+  let { restorationInverseTemperature, exchangeConstant, errorThreshold, iterationCount } =
+    ImageRestoration.DefaultParameters();
 
-	$: originalImage = buildOriginalImage()
-	$: degradedImage = new BinaryImage(imageSize.width, imageSize.height, true)
-	$: restoredImage = new BinaryImage(imageSize.width, imageSize.height, true)
+  $: originalImage = buildOriginalImage();
+  $: degradedImage = new BinaryImage(imageSize.width, imageSize.height, true);
+  $: restoredImage = new BinaryImage(imageSize.width, imageSize.height, true);
 
-	function degradeImage() {
-		degradedImage = originalImage.createDegradedImage(degradeProbability)
-	}
+  function degradeImage() {
+    degradedImage = originalImage.createDegradedImage(degradeProbability);
+  }
 
-	function restoreImage() {
-		const imageRestoration = new ImageRestoration(degradedImage)
+  function restoreImage() {
+    const imageRestoration = new ImageRestoration(degradedImage);
 
-		const restorationParameters = [
-			nishimoriInverseTemperature,
-			restorationInverseTemperature,
-			exchangeConstant,
-			errorThreshold
-		] as const
+    const restorationParameters = [
+      nishimoriInverseTemperature,
+      restorationInverseTemperature,
+      exchangeConstant,
+      errorThreshold
+    ] as const;
 
-		const result = imageRestoration.restore(...restorationParameters)
-		restoredImage = result.restoredImage
-		iterationCount = result.count
-	}
+    const result = imageRestoration.restore(...restorationParameters);
+    restoredImage = result.restoredImage;
+    iterationCount = result.count;
+  }
 
-	function resetImages() {
-		iterationCount = 0
-		degradedImage.reset()
-		restoredImage.reset()
-		degradedImage = degradedImage
-		restoredImage = restoredImage
-		originalImage = buildOriginalImage()
-	}
+  function resetImages() {
+    iterationCount = 0;
+    degradedImage.reset();
+    restoredImage.reset();
+    degradedImage = degradedImage;
+    restoredImage = restoredImage;
+    originalImage = buildOriginalImage();
+  }
 
-	function setTheme(event: ComponentEvents<ThemeSwitch>['theme-switch']) {
-		isThemeSwitched = event.detail.theme
-		originalImage = buildOriginalImage()
-	}
+  function setTheme(event: ComponentEvents<ThemeSwitch>['theme-switch']) {
+    isThemeSwitched = event.detail.theme;
+    originalImage = buildOriginalImage();
+  }
 
-	function buildOriginalImage(): BinaryImage {
-		const isWorkInProgress = !(degradedImage == undefined || degradedImage.isBlank())
+  function buildOriginalImage(): BinaryImage {
+    const isWorkInProgress = !(degradedImage == undefined || degradedImage.isBlank());
 
-		if (isWorkInProgress) return originalImage
+    if (isWorkInProgress) return originalImage;
 
-		if (isThemeSwitched) {
-			return BinaryImage.buildTreeImage(imageSize.width, imageSize.height)
-		} else {
-			return BinaryImage.buildBowImage(imageSize.width, imageSize.height)
-		}
-	}
+    if (isThemeSwitched) {
+      return BinaryImage.buildTreeImage(imageSize.width, imageSize.height);
+    } else {
+      return BinaryImage.buildBowImage(imageSize.width, imageSize.height);
+    }
+  }
 </script>
 
 <div>
-	<div class="util-panels">
-		<div class="panel">
-			<div class="title">パラメータ</div>
-			<div class="parameters">
-				<ParameterInput bind:parameterValue={degradeProbability} step={0.1}>劣化確率</ParameterInput>
-				<ParameterInput bind:parameterValue={nishimoriInverseTemperature} step={0.01} disabled={true}>西森逆温度</ParameterInput>
-				<ParameterInput bind:parameterValue={restorationInverseTemperature} step={0.1}>修復逆温度</ParameterInput>
-				<ParameterInput bind:parameterValue={exchangeConstant} step={0.1}>交換定数</ParameterInput>
-				<ParameterInput bind:parameterValue={errorThreshold} step={0.0001}>誤差</ParameterInput>
-				<ParameterInput bind:parameterValue={iterationCount} step={1} disabled={true}>反復回数</ParameterInput>
-			</div>
-		</div>
+  <div class="util-panels">
+    <div class="panel">
+      <div class="title">パラメータ</div>
+      <div class="parameters">
+        <ParameterInput bind:parameterValue={degradeProbability} step={0.1}>劣化確率</ParameterInput
+        >
+        <ParameterInput
+          bind:parameterValue={nishimoriInverseTemperature}
+          step={0.01}
+          disabled={true}>西森逆温度</ParameterInput
+        >
+        <ParameterInput bind:parameterValue={restorationInverseTemperature} step={0.1}
+          >修復逆温度</ParameterInput
+        >
+        <ParameterInput bind:parameterValue={exchangeConstant} step={0.1}>交換定数</ParameterInput>
+        <ParameterInput bind:parameterValue={errorThreshold} step={0.0001}>誤差</ParameterInput>
+        <ParameterInput bind:parameterValue={iterationCount} step={1} disabled={true}
+          >反復回数</ParameterInput
+        >
+      </div>
+    </div>
 
-		<div class="panel">
-			<div class="title">アクション</div>
-			<div class="actions">
-				<button type="button" on:click={degradeImage}>劣化させる</button>
-				<button type="button" on:click={restoreImage} disabled={degradedImage.isBlank()}>修復する</button>
-				<button type="button" on:click={resetImages}>画像をリセット</button>
-			</div>
-		</div>
-	</div>
+    <div class="panel">
+      <div class="title">アクション</div>
+      <div class="actions">
+        <button type="button" on:click={degradeImage}>劣化させる</button>
+        <button type="button" on:click={restoreImage} disabled={degradedImage.isBlank()}
+          >修復する</button
+        >
+        <button type="button" on:click={resetImages}>画像をリセット</button>
+      </div>
+    </div>
+  </div>
 
-	<div class="image-panel panel">
-		<div>
-			原画像
-			<Image image={originalImage} {...imageSize} />
-		</div>
-		<div>
-			劣化画像
-			<Image image={degradedImage} {...imageSize} />
-		</div>
-		<div>
-			修復画像
-			<Image image={restoredImage} {...imageSize} />
-		</div>
-	</div>
+  <div class="image-panel panel">
+    <div>
+      原画像
+      <Image image={originalImage} {...imageSize} />
+    </div>
+    <div>
+      劣化画像
+      <Image image={degradedImage} {...imageSize} />
+    </div>
+    <div>
+      修復画像
+      <Image image={restoredImage} {...imageSize} />
+    </div>
+  </div>
 </div>
 
 <div class="footer">
-	<ThemeSwitch on:theme-switch={setTheme} />
+  <ThemeSwitch on:theme-switch={setTheme} />
 
-	<Snow />
+  <Snow />
 </div>
 
 <style>
-	.util-panels {
-		display: flex;
-		flex-flow: row;
-	}
+  .util-panels {
+    display: flex;
+    flex-flow: row;
+  }
 
-	.panel {
-		display: flex;
-		flex-flow: row;
-		margin: 10px;
-		padding: 1rem;
-		box-shadow: 0 5px 10px 1px rgba(0, 0, 0, .14);
-	}
+  .panel {
+    display: flex;
+    flex-flow: row;
+    margin: 10px;
+    padding: 1rem;
+    box-shadow: 0 5px 10px 1px rgba(0, 0, 0, 0.14);
+  }
 
-	.title {
-		margin-right: 2rem;
-	}
+  .title {
+    margin-right: 2rem;
+  }
 
-	.parameters {
-		display: flex;
-		flex-flow: column;
-		align-items: flex-end;
-	}
+  .parameters {
+    display: flex;
+    flex-flow: column;
+    align-items: flex-end;
+  }
 
-	.actions {
-		display: flex;
-		flex-flow: column;
-	}
+  .actions {
+    display: flex;
+    flex-flow: column;
+  }
 
-	button {
-		width: 8rem;
-		margin: 5px;
-	}
+  button {
+    width: 8rem;
+    margin: 5px;
+  }
 
-	.image-panel {
-		flex-wrap: wrap;
-		justify-content: space-between;
-	}
+  .image-panel {
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
 
-	:global(body.silent-night) {
-		color: #E6E6E6;
-		background-color: #004459;
-	}
+  :global(body.silent-night) {
+    color: #e6e6e6;
+    background-color: #004459;
+  }
 
-	:global(body.silent-night) .panel {
-		box-shadow: 0 5px 10px 1px rgba(255, 255, 255, .14);
-	}
+  :global(body.silent-night) .panel {
+    box-shadow: 0 5px 10px 1px rgba(255, 255, 255, 0.14);
+  }
 
-	.footer {
-		padding: 0 .5rem;
-		padding-bottom: 5rem;
-	}
+  .footer {
+    padding: 0 0.5rem;
+    padding-bottom: 5rem;
+  }
 </style>
